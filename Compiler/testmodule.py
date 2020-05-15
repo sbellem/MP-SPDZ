3cea4fbf357d86366b6925c65d642f7224e68249
Andrew Miller <socrates1024@gmail.com>
2020-05-15 13:09:52 -0500

    Test to show importing modules

---
+++ Compiler/testmodule.py
@@ -0,0 +1,8 @@
+# Python modules in here can be imported from .mpc files
+
+from Compiler.util import *
+from Compiler.types import *
+from Compiler.library import *
+
+def test_module():
+    print_ln("hi")
