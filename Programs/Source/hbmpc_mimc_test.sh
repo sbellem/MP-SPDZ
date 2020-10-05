1c3b0cf0b1d6de98fbd005c48b21c038a2a47266
Sylvain Bellemare <sbellem@gmail.com>
2020-10-05 22:00:16 +0100

    Update hbmpc script to work with latest code

--- Programs/Source/hbmpc_mimc_test.sh
+++ Programs/Source/hbmpc_mimc_test.sh
@@ -21,25 +21,25 @@ echo 8 > Player-Data/Input-P2-0
 echo 0 > Player-Data/Input-P3-0
 
 
-# ## SPDZ style, n-of-n additive encoding
-# # Setup online
-# echo "Running Fake Offline Phase"
-# Scripts/setup-online.sh $players 256 128
-# # Run the Online
-# echo "SPDZ-style Online Phase"
-# for i in 0 1 2 3; do
-#     ./Player-Online.x -N 4 -t 1 -F -p $i -P $BLS_PRIME hbmpc_mimc_test  & pids[${i}]=$!
-# done
-# for pid in ${pids[*]}; do
-#     wait $pid
-# done
+## SPDZ style, n-of-n additive encoding
+# Setup online
+echo "Running Fake Offline Phase"
+Scripts/setup-online.sh $players 256 128
+# Run the Online
+echo "SPDZ-style Online Phase"
+for i in 0 1 2 3; do
+    ./Player-Online.x -N 4 -p $i -P $BLS_PRIME hbmpc_mimc_test  & pids[${i}]=$!
+done
+for pid in ${pids[*]}; do
+    wait $pid
+done
 
 progs="./malicious-shamir-party.x"
 # Run online
 for prog in $progs; do
     echo "Combined Offline/Online Experiment $prog"
     for i in 0 1 2 3; do
-	$prog -N 4 -t 1 -p $i -P $BLS_PRIME hbmpc_mimc_test  & pids[${i}]=$!
+	$prog -N 4 -T 1 -p $i -P $BLS_PRIME hbmpc_mimc_test  & pids[${i}]=$!
     done
     for pid in ${pids[*]}; do
 	wait $pid
