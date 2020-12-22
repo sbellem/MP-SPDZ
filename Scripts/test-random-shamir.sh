d5501981d21db6c03773135247c16f4189e387b7
Sylvain Bellemare <sbellem@gmail.com>
2020-12-22 00:01:17 +0000

    Attempt to fix passing Shamir Option threshold

--- Scripts/test-random-shamir.sh
+++ Scripts/test-random-shamir.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-./random-shamir.x -i 0 \
-    & ./random-shamir.x -i 1 \
-    & ./random-shamir.x -i 2 \
-    & ./random-shamir.x -i 3
+./random-shamir.x -i 0 -N 7 \
+    & ./random-shamir.x -i 1 -N 7 -T 3 \
+    & ./random-shamir.x -i 2 -N 7 -T 3 \
+    & ./random-shamir.x -i 3 -N 7 -T 3 \
+    & ./random-shamir.x -i 4 -N 7 -T 3 \
+    & ./random-shamir.x -i 5 -N 7 -T 3 \
+    & ./random-shamir.x -i 6 -N 7 -T 3
